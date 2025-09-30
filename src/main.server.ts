cc349affc746780b13fc65dd97ba5cb4f668c9bf
Pablo-Devs <blanksonpaul3@gmail.com>
2025-09-30 16:24:31 +0000

    style: format bootstrap function for improved readability

--- src/main.server.ts
+++ src/main.server.ts
@@ -3,6 +3,7 @@ import { App } from './app/app'
 import { config } from './app/app.config.server'
 import { ApplicationRef } from '@angular/core'
 
-const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> => bootstrapApplication(App, config, context)
+const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> =>
+  bootstrapApplication(App, config, context)
 
 export default bootstrap
