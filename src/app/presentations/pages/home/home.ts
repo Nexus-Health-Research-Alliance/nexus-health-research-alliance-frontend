65284a4375501dcdd2a786e96f37b27ff1af0f9d
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-10 18:41:26 +0000

    refactor: integrate button component

--- src/app/presentations/pages/home/home.ts
+++ src/app/presentations/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { ourPurpose, contentCard } from './../../../logics/constants/home-page.constants'
 import { Component } from '@angular/core'
 import { NgOptimizedImage } from '@angular/common'
-import { ContentCard, HeroSection } from '../../../../../projects/ui-components/src/public-api'
+import { ContentCard, HeroSection, Button } from '../../../../../projects/ui-components/src/public-api'
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, ContentCard, NgOptimizedImage],
+  imports: [HeroSection, ContentCard, Button, NgOptimizedImage],
   templateUrl: './home.html',
   styles: [
     `
