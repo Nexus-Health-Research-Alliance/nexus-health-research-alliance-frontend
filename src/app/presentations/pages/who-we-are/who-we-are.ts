1d670d419b3ca6c82804d43ebe28435653254392
Pablo-Devs <blanksonpaul3@gmail.com>
2025-11-15 08:41:08 +0000

    feat: implement who we are section of the about page

--- src/app/presentations/pages/who-we-are/who-we-are.ts
+++ src/app/presentations/pages/who-we-are/who-we-are.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { NgOptimizedImage } from '@angular/common'
 import { HeroSection } from '../../../../../projects/ui-components/src/public-api'
 
 @Component({
   selector: 'app-who-we-are',
-  imports: [HeroSection],
+  imports: [HeroSection, NgOptimizedImage],
   templateUrl: './who-we-are.html',
 })
 export class WhoWeAre {}
